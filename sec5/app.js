2774ea2bf7c0f168c47549331a01f4d94cf03293
nakamurusun <3872mn@gmail.com>
2019-12-31 09:34:34 +0900

    moved routing from app.js to routes dir

--- sec5/app.js
+++ sec5/app.js
@@ -1,31 +1,26 @@
 // const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 // bodyParser is now preinstalled with express.js,
 // but it's recommended to install manually to parse body without crush
 // even if this would be removed in the future.
 
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
 app.use('/', (req, res, next) => {
     // This always runs, because specifies a slash as a path and it is the first middleware.
     console.log('This always runs!')
     next(); // allows the req to continue to the next middleware in line
 });
-app.use('/add-product', (req, res, next) => {
-    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/2', (req, res, next) => {
-    console.log('<h1>Hello from express 2 !</h1>')
-})
 
 // const server = http.createServer(app);
 
