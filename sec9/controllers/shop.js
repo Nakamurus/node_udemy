c0edbe7ff336830c96499aaf6e5bfde2cd2306b1
nakamurusun <3872mn@gmail.com>
2020-01-05 22:30:56 +0900

    switched from raw SQL to Sequelize

--- sec9/controllers/shop.js
+++ sec9/controllers/shop.js
@@ -2,14 +2,30 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product // using Sequelize
+    .findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products'
       });
     })
+    .catch(err => {
+      console.log(err)
+    })
+
+  // using raw SQL
+  // Product.fetchAll()
+  //   .then(([rows, fieldData]) => {
+  //     res.render('shop/product-list', {
+  //       prods: rows,
+  //       pageTitle: 'All Products',
+  //       path: '/products'
+  //     });
+  //   })
+
+  // fetching from file
   // Product.fetchAll(products => {
   //   res.render('shop/product-list', {
   //     prods: products,
@@ -21,15 +37,28 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then(product => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
     })
     .catch(err => console.log(err));
+
+  // Using raw SQL
+  // Product.findById(prodId)
+  //   .then(([product]) => {
+  //     res.render('shop/product-detail', {
+  //       product: product[0],
+  //       pageTitle: product.title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+
+  // Fetching from file
   // Product.findById(prodId, products => {
   //   res.render('shop/product-detail', {
   //     product: product,
@@ -40,6 +69,31 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  Product // using Sequelize
+    .findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  // using raw SQL
+  // Product.fetchAll()
+  //   .then(([rows, fieldData]) => {
+  //     res.render('shop/index', {
+  //       prods: rows,
+  //       pageTitle: 'shop',
+  //       path: '/'
+  //          });
+  //   })
+  //   .catch(err => console.log(err));
+
+  // Fetching from file
   // Product.fetchAll(products => {
   //   res.render('shop/index', {
   //     prods: products,
@@ -47,15 +101,6 @@ exports.getIndex = (req, res, next) => {
   //     path: '/'
   //   });
   // });
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'shop',
-        path: '/'
-           });
-    })
-    .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
