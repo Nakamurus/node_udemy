83738e51c1a1366295f2beb93d2233da0eb0d1e5
nakamurusun <3872mn@gmail.com>
2020-01-05 14:24:35 +0900

    deprecated files which uses raw SQL

---
+++ sec9/util/databaseWithRawSQL.js
@@ -0,0 +1,12 @@
+// deprecated
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({ // pool of connection
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'ROSIAdekirusql1'
+});
+
+module.exports = pool.promise();
