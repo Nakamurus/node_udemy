56b513099a559b2702e5e88f48f5edfb027bd7cf
nakamurusun <3872mn@gmail.com>
2020-01-05 13:38:01 +0900

    using sequelize instead of raw mysql

--- sec9/util/database.js
+++ sec9/util/database.js
@@ -1,10 +1,23 @@
-const mysql = require('mysql2');
+// Without Sequelize version
 
-const pool = mysql.createPool({ // pool of connection
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'ROSIAdekirusql1'
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({ // pool of connection
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'ROSIAdekirusql1'
+// });
+
+// module.exports = pool.promise();
+
+// Now Sequelize version
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'node-complete', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
