ceb7c1427c5894eef5e340e9e1459cfcfffa248d
nakamurusun <3872mn@gmail.com>
2020-01-05 13:27:43 +0900

    connected with mysql and can add to database from site now

--- sec9/util/database.js
+++ sec9/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({ // pool of connection
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'ROSIAdekirusql1'
 });
 
 module.exports = pool.promise();
