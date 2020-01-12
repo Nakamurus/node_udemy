f975ca7970dd77f8e9ac38e440bbd0ce1d7167c8
nakamurusun <3872mn@gmail.com>
2020-01-12 11:07:37 +0900

    created login page

--- sec9/app.js
+++ sec9/app.js
@@ -16,7 +16,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
