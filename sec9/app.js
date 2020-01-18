e3336827212104b5cf48f15dd7d7c745fddb5b85
nakamurusun <3872mn@gmail.com>
2020-01-18 12:21:39 +0900

    added file handling instead of image URL using multer

--- sec9/app.js
+++ sec9/app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash'); // holds info and delete from a session after using it
-
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -23,6 +23,15 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -31,6 +40,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ storage: fileStorage }).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
