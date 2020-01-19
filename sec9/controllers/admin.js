51617eb96fd55e1cf1cc1347ae2f07b64cf1b782
nakamurusun <3872mn@gmail.com>
2020-01-19 11:57:55 +0900

    switch from entire reloading to async fetch deleting products

--- sec9/controllers/admin.js
+++ sec9/controllers/admin.js
@@ -172,8 +172,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId).then(prod => {
     if (!prod) {
       return next(new Error('Product not found.'));
@@ -183,11 +183,13 @@ exports.postDeleteProduct = (req, res, next) => {
   })
   .then(() => {
     console.log('DESTROYED PRODUCT');
-    res.redirect('/admin/products');
+    res.status(200).json({
+      message: 'Success!'
+    });
   })
   .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+    res.status(500).json({
+      message: 'Deleting product failed.'
+    });
   });
 };
