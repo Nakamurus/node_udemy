902e61530636c4b70a082cc53244950730941dd1
nakamurusun <3872mn@gmail.com>
2020-01-19 11:14:48 +0900

    added pagination

--- sec9/controllers/shop.js
+++ sec9/controllers/shop.js
@@ -6,21 +6,42 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  // "+" sets value as a number not as tring
+  // "||" in order to set 1 when there's not query
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+      .skip((page -1) * ITEMS_PER_PAGE) // set how many products to skip( = a lower limit of product number in the page)
+      .limit(ITEMS_PER_PAGE) // set an upper limit of number of product to show
+      // with SQL you can use LIMIT and OFFSET commands
+      //  https://stackoverflow.com/questions/3799193/mysql-data-best-way-to-implement-paging
+    })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+      })
+      .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
       });
     })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -41,12 +62,32 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  // "+" sets value as a number not as tring
+  // "||" in order to set 1 when there's not query
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+      .skip((page -1) * ITEMS_PER_PAGE) // set how many products to skip( = a lower limit of product number in the page)
+      .limit(ITEMS_PER_PAGE) // set an upper limit of number of product to show
+      // with SQL you can use LIMIT and OFFSET commands
+      //  https://stackoverflow.com/questions/3799193/mysql-data-best-way-to-implement-paging
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
