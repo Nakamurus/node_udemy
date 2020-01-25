cc3a06ac1b5c4a3f0acde8a6cdd3b74f2f0c5d39
nakamurusun <3872mn@gmail.com>
2020-01-25 10:29:51 +0900

    connected with mongoDB using mongoose

--- restAPI/app.js
+++ restAPI/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,6 +10,24 @@ const app = express();
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    // set these to avoid CORS(Cross Origin Resource Sharing) Errors
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT', 'PATCH', 'DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+    .connect(
+        process.env.MONGODBCONNECTIONSTR,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
--- restAPI/controllers/feed.js
+++ restAPI/controllers/feed.js
@@ -1,19 +1,43 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'First Post', conent: 'This is the first post!' }]
+        posts: [
+            {
+                _id: '1',
+                title: 'First Post',
+                conent: 'This is the first post!',
+                imageUrl: 'images/michelle-spollen-c9MFM8rSMsQ-unsplash.jpg',
+                creator: {
+                    name: 'Nakamura'
+                },
+                createdAt: new Date()
+            }
+        ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({
+                message: 'Validation failed, entered data is incorrect',
+                errors: errors.array()
+            })
+    }
     const title = req.body.title;
     const content = req.body.content;
     // create post in db
     res.status(201).json({
         message: 'Post created Successfully',
         post: {
-            id: new Date().toISOString().replace(/:/g,'-'),
+            _id: new Date().toISOString().replace(/:/g,'-'),
             title: title,
-            content: content
+            content: content,
+            creator: { name: 'Nakamura'},
+            createdAt: new Date()
         }
     });
 }
