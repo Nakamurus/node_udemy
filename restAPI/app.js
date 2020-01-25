cdf8dd6b7c3d13feea87e8e6d46ecd9b76788938
nakamurusun <3872mn@gmail.com>
2020-01-25 17:22:28 +0900

    made it available to post and get post detail and its list

--- restAPI/app.js
+++ restAPI/app.js
@@ -1,14 +1,46 @@
+const path = require('path');
+
 require('dotenv').config();
-const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'iamge/png' ||
+        file.mimetype === 'image.jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use(
+    multer({
+        storage: fileStorage,
+        fileFilter: fileFilter
+    })
+    .single('image') // the name of incoming request
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     // set these to avoid CORS(Cross Origin Resource Sharing) Errors
@@ -19,6 +51,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 mongoose
     .connect(
