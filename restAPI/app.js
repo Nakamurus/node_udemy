825ad6090c4680e40dc91b1635fec89c50e8e524
nakamurusun <3872mn@gmail.com>
2020-01-28 23:25:47 +0900

    now only a creator can edit and delete their post

--- restAPI/app.js
+++ restAPI/app.js
@@ -46,8 +46,14 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
     // set these to avoid CORS(Cross Origin Resource Sharing) Errors
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
     next();
 })
 
--- restAPI/controllers/feed.js
+++ restAPI/controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -16,7 +17,7 @@ exports.getPosts = (req, res, next) => {
                 .skip((currentPage - 1) * perPage)
                 .limit(perPage);
         })
-        .then(post => {
+        .then(posts => {
             res
               .status(200)
               .json(
@@ -25,12 +26,6 @@ exports.getPosts = (req, res, next) => {
                       posts: posts,
                       totalItems: totalItems
                   });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
         })
         .catch(err => {
             if (!err.statusCode) {
@@ -55,20 +50,29 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path.replace('\\', '/');
     const title = req.body.title;
     const content = req.body.content;
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: { name: 'Nakamura' }
+        creator: req.userId,
     });
     post
         .save()
         .then(result => {
-            console.log(result);
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
-        });
+                post: post,
+                creator: { _id: creator._id, name: creator.name }
+            });
         })
         .catch(err => {
             if (!err.statusCode) {
@@ -126,6 +130,11 @@ exports.updatePost = (req, res, next) => {
                 error.statusCode = 404;
                 throw error;
             }
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not authorized!')
+                error.statusCode = 403;
+                throw error;
+            }
             if (imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
             }
@@ -152,6 +161,11 @@ exports.deletePost = (req, res, next) => {
             if (!post) {
                 const error = new Error('Could not find post.');
                 error.statusCode = 404;
+                throw error;
+            }
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not authorized');
+                error.statusCode = 403;
                 throw error;
             }
             clearImage(post.imageUrl);
